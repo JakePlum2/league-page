915f248f0f4005dce0eb0e478580a156ec01fea4
JakePlum2 <jakeplummer2128@gmail.com>
2024-12-10 13:33:04 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -27,58 +27,128 @@ export const managers = [
     //   "name": "Ty Dorvinen",
     //   "tookOver": 2019, // (optional) used if a manager took over a team, delete this line or change to null otherwise
     //   "location": "Green Bay", // (optional)
-    //   "bio": "",
     //   "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
     //   "rival": {
     //     name: "Tai Thao", // Can be anything (usually your rival's name)
-    //     link: 6, // manager array number within this array, or null to link back to all managers page
+    //     link: 9, // manager array number within this array, or null to link back to all managers page
     //     image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
     //   },
     // },
     // {
     //   "roster": 2,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-    //   "name": "Your Name",
-    //   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-    //   "location": "Brooklyn", // (optional)
-    //   "bio": "Lorem ipsum...",
+    //   "name": "Jackson Salmon",
+    //   "tookOver": 2015, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+    //   "location": "Green Bay", // (optional)
     //   "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
-    //   "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
-    //   "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-    //   "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
     //   "rival": {
-    //     name: "Rival", // Can be anything (usually your rival's name)
-    //     link: 6, // manager array number within this array, or null to link back to all managers page
+    //     name: "Grant Maus", // Can be anything (usually your rival's name)
+    //     link: 8, // manager array number within this array, or null to link back to all managers page
     //     image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
     //   },
-    //   "favoritePlayer": 1426, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
-    //   "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
-    //   "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
-    //   "philosophy": "Your fantasy team's philosophy",
-    //   "tradingScale": 10, // 1 - 10
-    //   "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     // },
     // {
     //   "roster": 3,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-    //   "name": "Your Name",
-    //   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-    //   "location": "Brooklyn", // (optional)
-    //   "bio": "Lorem ipsum...",
+    //   "Colton Gaeth": "Your Name",
+    //   "tookOver": 2015, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+    //   "location": "Appleton", // (optional)
     //   "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
-    //   "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
-    //   "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-    //   "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
     //   "rival": {
-    //     name: "Rival", // Can be anything (usually your rival's name)
+    //     name: "Ryan Kemppainen", // Can be anything (usually your rival's name)
+    //     link: 4, // manager array number within this array, or null to link back to all managers page
+    //     image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+    //   },
+    // },
+      // {
+    //   "roster": 4,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+    //   "name": "Drake Toneys",
+    //   "tookOver": 2015, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+    //   "location": "Green Bay", // (optional)
+    //   "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+    //   "rival": {
+    //     name: "Jaden Wilson", // Can be anything (usually your rival's name)
+    //     link: 5, // manager array number within this array, or null to link back to all managers page
+    //     image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+    //   },
+    // },
+    // {
+      // {
+    //   "roster": 5,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+    //   "name": "Ryan Kemppainen",
+    //   "tookOver": 2015, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+    //   "location": "Madison", // (optional)
+    //   "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+    //   "rival": {
+    //     name: "Colton Gaeth", // Can be anything (usually your rival's name)
+    //     link: 2, // manager array number within this array, or null to link back to all managers page
+    //     image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+    //   },
+    // },
+    // {
+      // {
+    //   "roster": 6,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+    //   "name": "Jaden Wilson",
+    //   "tookOver": 2015, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+    //   "location": "Green Bay", // (optional)
+    //   "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+    //   "rival": {
+    //     name: "Drake Toneys", // Can be anything (usually your rival's name)
+    //     link: 3, // manager array number within this array, or null to link back to all managers page
+    //     image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+    //   },
+    // },
+    // {
+      // {
+    //   "roster": 7,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+    //   "name": "Sam Salemi",
+    //   "tookOver": 2016, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+    //   "location": "Green Bay", // (optional)
+    //   "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+    //   "rival": {
+    //     name: "Jake Plummer", // Can be anything (usually your rival's name)
+    //     link: 7, // manager array number within this array, or null to link back to all managers page
+    //     image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+    //   },
+    // },
+    // {
+      // {
+    //   "roster": 8,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+    //   "name": "Jake Plummer",
+    //   "tookOver": 2015, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+    //   "location": "Green Bay", // (optional)
+    //   "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+    //   "rival": {
+    //     name: "Sam Salemi", // Can be anything (usually your rival's name)
     //     link: 6, // manager array number within this array, or null to link back to all managers page
     //     image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
     //   },
-    //   "favoritePlayer": 1426, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
-    //   "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
-    //   "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
-    //   "philosophy": "Your fantasy team's philosophy",
-    //   "tradingScale": 10, // 1 - 10
-    //   "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     // },
+    // {
+      // {
+    //   "roster": 9,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+    //   "name": "Grant Maus",
+    //   "tookOver": 2015, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+    //   "location": "Green Bay", // (optional)
+    //   "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+    //   "rival": {
+    //     name: "Jackson Salmon", // Can be anything (usually your rival's name)
+    //     link: 1, // manager array number within this array, or null to link back to all managers page
+    //     image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+    //   },
+    // },
+    // {
+      // {
+    //   "roster": 10,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+    //   "name": "Tai Thao",
+    //   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+    //   "location": "Green Bay", // (optional)
+    //   "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+    //   "rival": {
+    //     name: "Ty Dorvinen", // Can be anything (usually your rival's name)
+    //     link: 0, // manager array number within this array, or null to link back to all managers page
+    //     image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+    //   },
+    // },
+    // {
   ]
   
   
